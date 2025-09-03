264724b106fc0db34a453480d2463ef334af4ef6
Amirsohail335 <amirsohail335@gmail.com>
2025-09-03 21:08:40 +0530

    get app users

--- dev-backend/src/app.js
+++ dev-backend/src/app.js
@@ -4,13 +4,34 @@ const User = require("./models/user");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 //Get user by email
+app.get("/user", async (req, res) => {
+  const UserEmail = req.body.emailId;
 
-app.get("/user", (req,res)=>{
-    const email = req.body.email
-})
+  try {
+    const users = await User.find({ emailId: UserEmail });
+    if (users.length == 0) {
+      res.send(403).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Get all users from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const user = new User({
