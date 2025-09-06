f52bb60a3b684c2e01b0ebb70917a95d77c34f8d
Amirsohail335 <amirsohail335@gmail.com>
2025-09-06 10:53:52 +0530

    normal user model

--- dev-backend/src/models/user.js
+++ dev-backend/src/models/user.js
@@ -3,20 +3,30 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
   },
   gender: {
+    type: String,
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
     type: String,
   },
 });
