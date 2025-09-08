d38b89be420bd59845a0f33f934cf21790e38cde
Amirsohail335 <amirsohail335@gmail.com>
2025-09-08 12:51:07 +0530

    committed

--- dev-backend/src/app.js
+++ dev-backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-
+const { validationSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 const app = express();
+const cookiesParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookiesParser());
 
 //Get user by email
 app.get("/user", async (req, res) => {
@@ -32,21 +35,65 @@ app.get("/feed", async (req, res) => {
     res.status(400).send("Something went wrong ");
   }
 });
+// SignUp User
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Ms",
-    lastName: "Dhoni",
-    emailId: "msd07@gmail.com",
-    password: "msd@777",
-  });
-
   try {
+    validationSignUpData(req);
+
+    // const user = new User(req.body);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    //Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    //Creating new instance
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
-    res.send("User added successfully");
-  } catch (err) {
-    res.status(400).send("Error is saving the user" + err.message);
+    res.send("User is successfully added");
+  } catch (error) {
+    res.status(400).send("Erro :" + error.message);
   }
+});
+
+//Login User
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).send("Invalid credentials Password");
+    }
+
+    // Add token to the call
+    res.cookie("token", "asdajhdhwierhe8383fdfjdnvjnv");
+
+    res.send("Login successful ✅");
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.send("Reading cookies");
 });
 
 connectDB()
