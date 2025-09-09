24102ee9a8a048b27fd1920e266847136b47a30b
Amirsohail335 <amirsohail335@gmail.com>
2025-09-09 23:40:13 +0530

    removed commented

--- dev-backend/src/models/user.js
+++ dev-backend/src/models/user.js
@@ -1,39 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     required: true,
-    
-//   },
-//   lastName: {
-//     type: String,
-//   },
-//   emailId: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   age: {
-//     type: Number,
-//   },
-//   gender: {
-//     type: String,
-//   },
-//   photoUrl: {
-//     type: String,
-//   },
-//   about: {
-//     type: String,
-//   },
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
 const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
