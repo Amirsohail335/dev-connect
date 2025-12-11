536d2ba6eda2332b7db96c8a2779908bf00f09d4
Amirsohail335 <amirsohail335@gmail.com>
2025-12-11 22:43:55 +0530

    checking token

--- dev-backend/src/middlewares/auth.js
+++ dev-backend/src/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
 // const adminAuth = (req, res, next) => {
 //   console.log("Admin auth is getting checked");
 
@@ -11,11 +13,28 @@ const jwt = require("jsonwebtoken");
 //   }
 // };
 
-const userAuth = (req, res, next) => {
+const userAuth = async (req, res, next) => {
   //Read the token from the req cookies
-  const { token } = req.cookies;
+  try {
+    const { token } = req.cookies;
+    if(!token){
+      throw new Error("Token is not valid")
+    }
 
-  const decodedObj = jwt.verify(token, "DEV@Tinder@790");
+    const decodedObj = await jwt.verify(token, "DEV@Tinder@790");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    next();
+  } catch (error) {
+    res.status(400).send("ERROR:" + error.message);
+  }
 };
 module.exports = {
   // adminAuth,
