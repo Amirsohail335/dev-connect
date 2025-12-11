0c85ac5169730106a498529eaf488c0a4ca1171d
Amirsohail335 <amirsohail335@gmail.com>
2025-12-11 22:34:47 +0530

    userAuth middleware

--- dev-backend/src/middlewares/auth.js
+++ dev-backend/src/middlewares/auth.js
@@ -1,15 +1,23 @@
-const adminAuth = (req, res, next) => {
-  console.log("Admin auth is getting checked");
+const jwt = require("jsonwebtoken");
+// const adminAuth = (req, res, next) => {
+//   console.log("Admin auth is getting checked");
 
-  const token = "xyz";
-  const authorisedToken = token === "xyz";
-  if (!authorisedToken) {
-    res.status(401).send("Unauthorised Access");
-  } else {
-    next();
-  }
+//   const token = "xyz";
+//   const authorisedToken = token === "xyz";
+//   if (!authorisedToken) {
+//     res.status(401).send("Unauthorised Access");
+//   } else {
+//     next();
+//   }
+// };
+
+const userAuth = (req, res, next) => {
+  //Read the token from the req cookies
+  const { token } = req.cookies;
+
+  const decodedObj = jwt.verify(token, "DEV@Tinder@790");
 };
-
 module.exports = {
-  adminAuth,
+  // adminAuth,
+  userAuth,
 };
