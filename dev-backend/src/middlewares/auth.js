567c3e30fb55a41c409e249ae14c3f3d06904810
Amirsohail335 <amirsohail335@gmail.com>
2025-12-12 21:47:01 +0530

    added authentication

--- dev-backend/src/middlewares/auth.js
+++ dev-backend/src/middlewares/auth.js
@@ -1,24 +1,11 @@
 const jwt = require("jsonwebtoken");
-
 const User = require("../models/user");
-// const adminAuth = (req, res, next) => {
-//   console.log("Admin auth is getting checked");
-
-//   const token = "xyz";
-//   const authorisedToken = token === "xyz";
-//   if (!authorisedToken) {
-//     res.status(401).send("Unauthorised Access");
-//   } else {
-//     next();
-//   }
-// };
 
 const userAuth = async (req, res, next) => {
-  //Read the token from the req cookies
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Token is not valid");
+      return res.status(401).send("Please Login!");
     }
 
     const decodedObj = await jwt.verify(token, "DEV@Tinder@790");
@@ -26,17 +13,17 @@ const userAuth = async (req, res, next) => {
     const { _id } = decodedObj;
 
     const user = await User.findById(_id);
-
     if (!user) {
       throw new Error("User not found");
     }
+
     req.user = user;
     next();
-  } catch (error) {
-    res.status(400).send("ERROR:" + error.message);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
+
 module.exports = {
-  // adminAuth,
   userAuth,
 };
