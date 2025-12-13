623888ae09fe053eb146aaf02e6ea836abd4be53
Amirsohail335 <amirsohail335@gmail.com>
2025-12-13 16:25:29 +0530

    Request Route added

---
+++ dev-backend/src/routes/request.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const requestRouter = express.Router();
+
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection Request sent Successfully",
+        data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR:" + error.message);
+    }
+
+    res.send(user.firstName + "send Connection req ");
+  }
+);
+
+module.exports = requestRouter;
