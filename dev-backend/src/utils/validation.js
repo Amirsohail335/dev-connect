c6489edb9b0f2155eccaee0b0720fb7f2d54dff5
Amirsohail335 <amirsohail335@gmail.com>
2025-12-14 22:11:37 +0530

    added validation

--- dev-backend/src/utils/validation.js
+++ dev-backend/src/utils/validation.js
@@ -32,6 +32,26 @@ const validationSignUpData = (req) => {
   }
 };
 
-module.exports={
-    validationSignUpData
-}
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
+module.exports = {
+  validationSignUpData,
+  validateEditProfileData,
+};
