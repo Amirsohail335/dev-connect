55e27d0c71303ba68f93be595bf75b7b3b6f5cf6
Amirsohail335 <amirsohail335@gmail.com>
2025-12-14 22:10:48 +0530

    Added user Routes

--- dev-backend/src/app.js
+++ dev-backend/src/app.js
@@ -1,144 +1,46 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User = require("./models/user");
-const { validationSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const app = express();
 const cookiesParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-
-const { userAuth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookiesParser());
 
+//Routes
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
 //Get user by email
-app.get("/user", async (req, res) => {
-  const UserEmail = req.body.emailId;
+// app.get("/user", async (req, res) => {
+//   const UserEmail = req.body.emailId;
 
-  try {
-    const users = await User.find({ emailId: UserEmail });
-    if (users.length == 0) {
-      res.send(403).send("User not found!");
-    } else {
-      res.send(users);
-    }
-  } catch (error) {
-    res.status(400).send("Something went wrong!!!!");
-  }
-});
-
-//Get all users from the database
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(400).send("Something went wrong!!!!");
-  }
-});
-// SignUp User
-
-app.post("/signup", async (req, res) => {
-  try {
-    validationSignUpData(req);
-
-    // const user = new User(req.body);
-
-    const { firstName, lastName, emailId, password } = req.body;
-    //Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
-
-    //Creating new instance
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
-
-    await user.save();
-    res.send("User is successfully added");
-  } catch (error) {
-    res.status(400).send("Erro :" + error.message);
-  }
-});
-
-//Login User
-
-app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-
-    // Check if user exists
-    const user = await User.findOne({ emailId: emailId });
-    if (!user) {
-      // return res.status(400).send("User not found");
-      throw new Error("Invalid Credetials");
-    }
-
-    // Compare password
-    const isPasswordValid = await user.validatePassword(password);
-    if (isPasswordValid) {
-      const token = await user.getJWT();
-      //Add token to Cookies and send response back to user
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000),
-      });
-      res.send("Login successful ✅");
-    } else {
-      throw new Error("Invalid Loginn Credentials");
-    }
-  } catch (error) {
-    res.status(400).send("Error: " + error.message);
-  }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("ERRORR :" + error.message);
-  }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  //Sending a connection Request
-  console.log("Sending a connection request");
-
-  res.send(user.firstName + "send Connection req ");
-});
-
-// app.get("/profile", userAuth, async (req, res) => {
 //   try {
-//     const cookies = req.cookies;
-//     const { token } = cookies;
-//     // console.log(cookies);
-//     if (!token) {
-//       throw new Error("Invalid Credentials");
+//     const users = await User.find({ emailId: UserEmail });
+//     if (users.length == 0) {
+//       res.send(403).send("User not found!");
+//     } else {
+//       res.send(users);
 //     }
-
-//     //Validate my token
-
-//     const decodedMessage = await jwt.verify(token, "DEV@Tinder@790");
-
-//     const { _id } = decodedMessage;
-
-//     console.log("Logged In user is" + _id);
-
-//     const user = User.findById(_id);
-//     if (!user) {
-//       throw new Error("User Does not exist");
-//     }
-
-//     // res.send("Reading cookies");
-//     res.send(user);
 //   } catch (error) {
-//     res.status(400).send("ERROR :" + error.message);
+//     res.status(400).send("Something went wrong!!!!");
+//   }
+// });
+
+// //Get all users from the database
+
+// app.get("/feed", async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.send(users);
+//   } catch (error) {
+//     res.status(400).send("Something went wrong!!!!");
 //   }
 // });
 
