01e994c8da646464b83631f4efcc9731bb58dbd9
Amirsohail335 <amirsohail335@gmail.com>
2025-12-25 11:33:26 +0530

    validation

--- dev-backend/src/utils/validation.js
+++ dev-backend/src/utils/validation.js
@@ -1,34 +1,71 @@
+// const validator = require("validator");
+
+// const validationSignUpData = (req) => {
+//   const { firstName, lastName, emailId, password } = req.body;
+
+//   // Validate firstName and lastName
+//   if (!firstName || !lastName) {
+//     throw new Error("Name is not valid");
+//   } else if (firstName.length < 4 || firstName.length > 50) {
+//     throw new Error("First name should be between 4-50 characters");
+//   }
+
+//   // Validate email using validator
+//   if (!validator.isEmail(emailId)) {
+//     throw new Error("Invalid email address");
+//   }
+
+//   // You can also validate password strength if needed
+//   // Validate password strength
+//   if (
+//     !validator.isStrongPassword(password, {
+//       minLength: 8,
+//       minLowercase: 1,
+//       minUppercase: 1,
+//       minNumbers: 1,
+//       minSymbols: 1,
+//     })
+//   ) {
+//     throw new Error(
+//       "Password must be at least 8 characters long and include 1 lowercase, 1 uppercase, 1 number, and 1 symbol"
+//     );
+//   }
+// };
+
+// const validateEditProfileData = (req) => {
+//   const allowedEditFields = [
+//     "firstName",
+//     "lastName",
+//     "emailId",
+//     "photoUrl",
+//     "gender",
+//     "age",
+//     "about",
+//     "skills",
+//   ];
+
+//   const isEditAllowed = Object.keys(req.body).every((field) =>
+//     allowedEditFields.includes(field)
+//   );
+
+//   return isEditAllowed;
+// };
+
+// module.exports = {
+//   validationSignUpData,
+//   validateEditProfileData,
+// };
+
 const validator = require("validator");
 
-const validationSignUpData = (req) => {
+const validateSignUpData = (req) => {
   const { firstName, lastName, emailId, password } = req.body;
-
-  // Validate firstName and lastName
   if (!firstName || !lastName) {
-    throw new Error("Name is not valid");
-  } else if (firstName.length < 4 || firstName.length > 50) {
-    throw new Error("First name should be between 4-50 characters");
-  }
-
-  // Validate email using validator
-  if (!validator.isEmail(emailId)) {
-    throw new Error("Invalid email address");
-  }
-
-  // You can also validate password strength if needed
-  // Validate password strength
-  if (
-    !validator.isStrongPassword(password, {
-      minLength: 8,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1,
-      minSymbols: 1,
-    })
-  ) {
-    throw new Error(
-      "Password must be at least 8 characters long and include 1 lowercase, 1 uppercase, 1 number, and 1 symbol"
-    );
+    throw new Error("Name is not valid!");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong Password!");
   }
 };
 
@@ -52,6 +89,6 @@ const validateEditProfileData = (req) => {
 };
 
 module.exports = {
-  validationSignUpData,
+  validateSignUpData,
   validateEditProfileData,
 };
