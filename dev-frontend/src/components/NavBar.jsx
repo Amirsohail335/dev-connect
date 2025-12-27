471ebd213947dbb54a1c58c4712b5d800ee9fab7
Amirsohail335 <amirsohail335@gmail.com>
2025-12-27 21:56:21 +0530

    navbar

--- dev-frontend/src/components/NavBar.jsx
+++ dev-frontend/src/components/NavBar.jsx
@@ -1,25 +1,43 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user, "userrr");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+      dispatch(removeUser());
+      return navigate("/login");
+    } catch (err) {
+      // Error logic maybe redirect to error page
+      console.log(err);
+    }
+  };
+
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Dev Connect</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          DevConnect 💻🤝
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
-          <div className="form-control ">Welcome , {user.firstName}</div>
-          <div className="dropdown dropdown-end mx-10 flex ">
+          <div className="form-control">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end mx-5 flex">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img alt="User Photo" src={user.photoUrl} />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -27,16 +45,23 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link to="/connections">Connections</Link>
+              </li>
+
+              <li>
+                <Link to="/requests">Requests</Link>
               </li>
               <li>
-                <a>Logout</a>
+                <Link to="/premium">Premium</Link>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
@@ -45,5 +70,4 @@ const NavBar = () => {
     </div>
   );
 };
-
 export default NavBar;
